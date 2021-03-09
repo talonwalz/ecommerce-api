0f7bb1ccb83c0153764d134ca0131b487d0ff63e
Talon Walz <78514227+talonwalz@users.noreply.github.com>
2021-03-09 16:01:41 -0700

    first API done

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const getProducts = require('./getProducts')
+const getProduct = require('./getProduct')
+
+const app = express() 
+
+const port = 3333;
+
+app.get('/api/products', getProducts)
+
+app.get('/api/products/:id', getProduct)
+
+
+app.listen(port, () => console.log(`I am running on ${port}`))
+
+
+
+
+
